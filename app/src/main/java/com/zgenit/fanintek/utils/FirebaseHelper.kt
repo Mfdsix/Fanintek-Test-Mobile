cb31c076d70d8adc9e28814dce39fac2a482f0a3
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-10-10 11:23:54 +0700

    remove unused file

--- app/src/main/java/com/zgenit/fanintek/utils/FirebaseHelper.kt
+++
@@ -1,9 +0,0 @@
-package com.zgenit.fanintek.utils
-
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-
-object FirebaseHelper {
-    val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
-    val firebaseUser: FirebaseUser? = firebaseAuth.currentUser
-}
